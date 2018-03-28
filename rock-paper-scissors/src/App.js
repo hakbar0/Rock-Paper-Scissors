ae8855f1d6de77f6754a8c77871d9eade86699aa
Haseeb Akbar <haseeb2000@hotmail.com>
2018-03-28 11:51:19 +0100

    Made a react app and setup template on where features should go.

---
+++ rock-paper-scissors/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div id="wrap">
+        <div class="title"></div>
+        <div class="instructions"></div>
+
+
+          <div class="player1"></div>
+          <div class="score-board"></div>
+          <div class="player2"></div>
+
+
+        <div class="footer"></div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
